8cafc1431d32395bc44c7d08e8630e1c82279325
ThilakSamplegithub <112916543+ThilakSamplegithub@users.noreply.github.com>
2024-06-28 19:39:42 +0530

    removing cross origin opener policy which are not allowing google auth

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ const { productRouter } = require('./Controller/product.route.js')
 const app=express()
 require('dotenv').config()
 app.use(cors())
+app.use((req, res, next) => {
+    res.removeHeader('Cross-Origin-Opener-Policy'); // Remove the header
+    // Alternatively, adjust the header
+    // res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+    // res.setHeader('Cross-Origin-Opener-Policy', 'unsafe-none');
+    next();
+  });
 app.use(express.json())
 // console.log(`${process.env.MONGODB_URL}`)
 app.get('/',(req,res)=>{
-    res.send('welcome')
+    res.send('welcome to real world')
 })
 
 app.use('/products',productRouter)
